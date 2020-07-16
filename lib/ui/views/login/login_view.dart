7083cde1cc794feda905e8674076aa1a2cc34569
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-16 22:34:18 +0800

    ...

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,37 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:laradart/ui/views/login/login_viewmodel.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:stacked/stacked.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _form = GlobalKey<FormState>();
+    final _email = TextEditingController(text: 'admin@admin.com');
+    final _password = TextEditingController(text: 'password');
+
     return ViewModelBuilder<LoginViewModel>.reactive(
       viewModelBuilder: () => LoginViewModel(),
       builder: (context, model, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Login'),
-          ),
-          body: ListView(
-            children: [
-              Container(
-                height: 300,
-                color: Colors.blue,
-                child: Column(
-                  children: [
-                    Text(model.loggedIn ? 'logged in' : 'not authenticated'),
-                    RaisedButton(
-                      child: Text('delete token'),
-                      onPressed: () => model.deleteToken(),
-                    ),
-                    RaisedButton(
-                      onPressed: () => model.login(),
-                      child: Text('Login!'),
-                    )
-                  ],
+        return ScreenTypeLayout(
+          mobile: SafeArea(
+            child: Scaffold(
+              body: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                        'https://images.pexels.com/photos/1680140/pexels-photo-1680140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                      ),
+                      fit: BoxFit.cover),
+                ),
+                child: Form(
+                  key: _form,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: TextFormField(
+                          controller: _email,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: TextFormField(
+                          controller: _password,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 50,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          onPressed: () {
+                            if (_form.currentState.validate()) {
+                              model.login();
+                            }
+                          },
+                          child: Text('LOGIN'),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ],
+            ),
           ),
         );
       },
