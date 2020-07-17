ae1188862cb7573c1d6618a1e53fa123d9778b6d
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-17 14:11:28 +0800

    refactor(alertService): created an AlertService so snackbar is accessible globally with the same styling

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:laradart/ui/views/home/home_view.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:observable_ish/observable_ish.dart';
@@ -13,6 +12,7 @@ import '../datamodels/auth_response.dart';
 import '../datamodels/user.dart';
 import '../app/routes.gr.dart';
 import '../app/locator.dart';
+import 'alert_service.dart';
 
 @lazySingleton
 class AuthenticationService with ReactiveServiceMixin {
@@ -20,7 +20,7 @@ class AuthenticationService with ReactiveServiceMixin {
   /// other classes that this [AuthenticationService] requires.
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   final _navigationService = locator<NavigationService>();
-  final _snackbarService = locator<SnackbarService>();
+  final _alertService = locator<AlertService>();
 
   AuthenticationService() {
     listenToReactiveValues([_token]);
@@ -49,8 +49,9 @@ class AuthenticationService with ReactiveServiceMixin {
       String token = prefs.getString(_authTokenName);
       _token.value = token;
 
+      await fetchUser();
       token.isNotEmpty
-          ? _navigationService.pushNamedAndRemoveUntil(Routes.homeView)
+          ? _navigationService.pushNamedAndRemoveUntil(Routes.tabLayoutView)
           : _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
     } else {
       _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
@@ -75,11 +76,12 @@ class AuthenticationService with ReactiveServiceMixin {
           AuthenticationResponse.fromJson(response.data);
       _token.value = data.accessToken;
       setToken(data.accessToken);
-      showSuccessSnackbar(
+      await fetchUser();
+      _alertService.showSnackbar(
         message: "You have logged in",
-        icon: Icons.check_circle_outline,
+        type: SnackBarType.SUCCESS,
       );
-      _navigationService.pushNamedAndRemoveUntil(Routes.homeView);
+      _navigationService.pushNamedAndRemoveUntil(Routes.tabLayoutView);
 
       return response;
     } on DioError catch (e) {
@@ -105,13 +107,57 @@ class AuthenticationService with ReactiveServiceMixin {
           AuthenticationResponse.fromJson(response.data);
       _token.value = data.accessToken;
       setToken(data.accessToken);
-      showSuccessSnackbar(
+      await fetchUser();
+      _alertService.showSnackbar(
         message: "You have logged in",
-        icon: Icons.check_circle_outline,
+        type: SnackBarType.SUCCESS,
       );
-      _navigationService.pushNamedAndRemoveUntil(Routes.homeView);
+      _navigationService.pushNamedAndRemoveUntil(Routes.tabLayoutView);
 
       return response;
+    } on DioError catch (e) {
+      handleError(e);
+    }
+  }
+
+  /// Fetch the current authenticated user.
+  ///
+  /// @return void
+  Future fetchUser() async {
+    try {
+      Response response = await dio.get(
+        '/api/auth/user',
+        options: Options(
+          headers: {"Authorization": "Bearer ${_token.value}"},
+        ),
+      );
+
+      User data = User.fromJson(response.data);
+      _user.value = data;
+    } on DioError catch (e) {
+      handleError(e);
+    }
+  }
+
+  /// Logs out the user.
+  ///
+  /// @return void
+  Future logout() async {
+    try {
+      await dio.get(
+        '/api/auth/logout',
+        options: Options(
+          headers: {"Authorization": "Bearer ${_token.value}"},
+        ),
+      );
+
+      deleteToken();
+      _user.value = User();
+      _alertService.showSnackbar(
+        message: "You have logged out.",
+        type: SnackBarType.INFO,
+      );
+      _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
     } on DioError catch (e) {
       handleError(e);
     }
@@ -147,94 +193,46 @@ class AuthenticationService with ReactiveServiceMixin {
 
     switch (error.response.statusCode) {
       case 403:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message:
               "You do not have the right privileges to access this resource.",
+          type: SnackBarType.ERROR,
         );
         _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
         break;
       case 422:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message:
               "Your credentials are wrong. Please try again using the correct credentials.",
+          type: SnackBarType.ERROR,
         );
         break;
       case 401:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message: "Incorrect credentials.",
+          type: SnackBarType.ERROR,
         );
         _navigationService.pushNamedAndRemoveUntil(Routes.loginView);
         break;
       case 404:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message: "Request not found.",
+          type: SnackBarType.ERROR,
         );
         break;
       case 500:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message:
               "There is something wrong with our servers, please report to the admin so it gets fixed.",
+          type: SnackBarType.ERROR,
         );
         break;
       default:
-        showErrorSnackbar(
-          icon: Icons.error_outline,
+        _alertService.showSnackbar(
           message: "An error has occurred anonymously.",
+          type: SnackBarType.ERROR,
         );
     }
-  }
-
-  /// Show error [Snackbar]
-  ///
-  /// @param [String] message
-  /// @param [IconData] icon
-  /// @return void
-  void showErrorSnackbar({@required String message, IconData icon}) {
-    _snackbarService.showCustomSnackBar(
-      message: message,
-      borderRadius: 15,
-      icon: Icon(
-        icon,
-        color: Colors.red,
-      ),
-      margin: EdgeInsets.all(15),
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-      shouldIconPulse: false,
-      snackStyle: SnackStyle.FLOATING,
-      snackPosition: SnackPosition.BOTTOM,
-      dismissDirection: SnackDismissDirection.HORIZONTAL,
-      duration: const Duration(seconds: 2),
-      isDismissible: true,
-    );
-  }
-
-  /// Show error [Snackbar]
-  ///
-  /// @param [String] message
-  /// @param [IconData] icon
-  /// @return void
-  void showSuccessSnackbar({@required String message, IconData icon}) {
-    _snackbarService.showCustomSnackBar(
-      message: message,
-      borderRadius: 15,
-      icon: Icon(
-        icon,
-        color: Colors.green,
-      ),
-      margin: EdgeInsets.all(15),
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-      shouldIconPulse: false,
-      snackStyle: SnackStyle.FLOATING,
-      snackPosition: SnackPosition.BOTTOM,
-      dismissDirection: SnackDismissDirection.HORIZONTAL,
-      duration: const Duration(seconds: 2),
-      isDismissible: true,
-    );
   }
 }
 
