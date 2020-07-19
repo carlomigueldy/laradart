d58c971bb29eb8c701f9c4bcf5db02411a6dcc38
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-19 14:33:51 +0800

    ui(themeService): theme is now toggable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import './app/locator.dart';
 import './app/routes.gr.dart';
@@ -13,16 +15,29 @@ void main() {
 class LaraDartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LaraDart Starter',
-      themeMode: locator<ThemeService>().theme,
-      theme: locator<ThemeService>().isLight
-          ? locator<ThemeService>().lightTheme
-          : locator<ThemeService>().darkTheme,
-      navigatorKey: locator<NavigationService>().navigatorKey,
-      onGenerateRoute: Router().onGenerateRoute,
-      initialRoute: Routes.splashView,
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => LaraDartViewModel(),
+      builder: (context, model, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'LaraDart Starter',
+        themeMode: model.themeMode,
+        theme: model.themeData,
+        navigatorKey: locator<NavigationService>().navigatorKey,
+        onGenerateRoute: Router().onGenerateRoute,
+        initialRoute: Routes.splashView,
+      ),
     );
   }
+}
+
+@singleton
+class LaraDartViewModel extends ReactiveViewModel {
+  final _themeService = locator<ThemeService>();
+
+  ThemeMode get themeMode => _themeService.theme;
+  ThemeData get themeData =>
+      _themeService.isDark ? _themeService.lightTheme : _themeService.darkTheme;
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [_themeService];
 }
