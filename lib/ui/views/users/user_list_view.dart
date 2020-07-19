8ca484bd5a799682a0f29745c9e9cd970a5c3121
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-19 14:35:59 +0800

    ...

--- lib/ui/views/users/user_list_view.dart
+++ lib/ui/views/users/user_list_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../datamodels/user.dart';
 import '../../widgets/page_header.dart';
 import '../../../app/locator.dart';
 import 'user_list_viewmodel.dart';
@@ -13,7 +14,6 @@ class UserListView extends StatelessWidget {
       viewModelBuilder: () => locator<UserListViewModel>(),
       initialiseSpecialViewModelsOnce: true,
       disposeViewModel: false,
-      onModelReady: (model) => model.initialize(),
       builder: (context, model, child) {
         return _UserListViewBody(model: model);
       },
@@ -28,90 +28,26 @@ class _UserListViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        PageHeader(
-          title: 'Users',
-        ),
-        Card(
-          margin: EdgeInsets.all(10),
-          child: SingleChildScrollView(
-            child: DataTable(
-              sortAscending: true,
-              showCheckboxColumn: true,
-              sortColumnIndex: 0,
-              onSelectAll: (value) {
-                print(value);
-              },
-              columns: const [
-                DataColumn(
-                    label: Text('Name'), tooltip: 'The name of the user'),
-                DataColumn(label: Text('Email')),
-                DataColumn(label: Text('Role')),
-                DataColumn(label: Text('Actions')),
-              ],
-              rows: [
-                DataRow(
-                  onSelectChanged: (value) => print(value),
-                  cells: [
-                    DataCell(Text('Carlo Miguel Dy')),
-                    DataCell(Text('carlomigueldy@gmail.com')),
-                    DataCell(Text('super-admin')),
-                    DataCell(
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () {},
-                            splashRadius: 20,
-                          )
-                        ],
-                      ),
-                      placeholder: true,
-                    ),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('Carlo Miguel Dy')),
-                    DataCell(Text('carlomigueldy@gmail.com')),
-                    DataCell(Text('super-admin')),
-                    DataCell(
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () {},
-                            splashRadius: 20,
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('Carlo Miguel Dy')),
-                    DataCell(Text('carlomigueldy@gmail.com')),
-                    DataCell(Text('super-admin')),
-                    DataCell(
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: Icon(CupertinoIcons.eye_solid),
-                            onPressed: () {},
-                            splashRadius: 20,
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
+    return !model.isBusy
+        ? ListView(
+            children: [
+              PageHeader(
+                title: 'Users',
+              ),
+              ...userList(),
+            ],
+          )
+        : Center(child: CircularProgressIndicator());
+  }
+
+  List<ListTile> userList() {
+    return model.data.map((User user) {
+      return ListTile(
+        title: Text(user.fullName),
+        subtitle: Text(user.email),
+        trailing: Icon(Icons.more_vert),
+        onTap: () => model.navigateToDetail(userId: user.id),
+      );
+    }).toList();
   }
 }
--- lib/ui/views/users/user_list_viewmodel.dart
+++ lib/ui/views/users/user_list_viewmodel.dart
@@ -1,26 +1,43 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
+import '../../../app/config.dart';
+import '../../../datamodels/user.dart';
 import '../../../app/locator.dart';
-import '../../../services/alert_service.dart';
+import '../../../app/routes.gr.dart';
 import '../../../services/authentication_service.dart';
 
 @singleton
-class UserListViewModel extends ReactiveViewModel {
+class UserListViewModel extends FutureViewModel<List<User>> {
   final _authService = locator<AuthenticationService>();
-  final _alertService = locator<AlertService>();
+  final _router = locator<NavigationService>();
 
   @override
-  List<ReactiveServiceMixin> get reactiveServices => [_authService];
+  Future<List<User>> futureToRun() async {
+    try {
+      print('[UserListViewModel] futureToRun');
+      setBusy(true);
+      Response response = await dio.get(
+        '/api/users',
+        options: _authService.authorizationHeader(),
+      );
+      setBusy(false);
 
-  initialize() {
-    print('[UserListViewModel] init');
+      List<User> users = [];
+
+      for (var item in response.data['data']) {
+        users.add(User.fromJson(item));
+      }
+
+      return users;
+    } on DioError catch (error) {
+      _authService.handleError(error);
+    }
   }
 
-  showSnackbar() {
-    _alertService.showSnackbar(
-      message: "Snackbar called!",
-      type: SnackBarType.INFO,
-    );
+  navigateToDetail({int userId}) {
+    _router.navigateTo(Routes.userDetailView(id: userId));
   }
 }
